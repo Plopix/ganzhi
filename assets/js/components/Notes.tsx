97af03922545af67a1d22b08455e1a5935706381
archalaus <78627931+archalaus@users.noreply.github.com>
2021-02-09 00:33:39 -0800

    Create Notes.tsx

---
+++ assets/js/components/Notes.tsx
@@ -0,0 +1,11 @@
+import React, {FunctionComponent} from 'react';
+import {translator} from "../Translator";
+
+const Notes: FunctionComponent = () => {
+    return <div className={'container text-justify'}>
+        <h1 dangerouslySetInnerHTML={{__html: translator.t('title', 'notes')}} />
+        <div dangerouslySetInnerHTML={{__html: translator.t('text', 'notes')}} />
+    </div>;
+}
+
+export default Notes;
