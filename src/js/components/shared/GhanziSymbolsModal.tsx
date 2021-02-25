5ebb6feb5fe3f78262507f1f0a8e9ccae7a821b6
Morel Sébastien <morel.seb@gmail.com>
2021-02-24 20:04:52 -0800

    Fix index issue

--- src/js/components/shared/GhanziSymbolsModal.tsx
+++ src/js/components/shared/GhanziSymbolsModal.tsx
@@ -8,6 +8,7 @@ import { SymbolsMap, symbolsmap } from '../../symbolsmap';
 const GanzhiSymbolsModal: FunctionComponent = () => {
     const [state] = useApp();
     const rank: number = GetRank(state.dayOfYear < 36 ? state.year - 1 : state.year);
+    const rankIndex: number = rank - 1;
     const map: SymbolsMap = symbolsmap[translator.locale];
     return (
         <>
@@ -18,13 +19,15 @@ const GanzhiSymbolsModal: FunctionComponent = () => {
             </Modal.Header>
             <Modal.Body className={'ganzhi-symbols'}>
                 <p className={'text-center'}>
-                    {map.gan[rank.realModulo(map.gan.length)][0]} {map.zhi[rank.realModulo(map.zhi.length)][0]} <br />
-                    {map.gan[rank.realModulo(map.gan.length)][1]} {map.zhi[rank.realModulo(map.zhi.length)][1]}
+                    {map.gan[rankIndex.realModulo(map.gan.length)][0]}{' '}
+                    {map.zhi[rankIndex.realModulo(map.zhi.length)][0]} <br />
+                    {map.gan[rankIndex.realModulo(map.gan.length)][1]}{' '}
+                    {map.zhi[rankIndex.realModulo(map.zhi.length)][1]}
                 </p>
                 <p className={'text-center'}>
                     <strong>{translator.t('symbol')}</strong>
                     <br />
-                    {map.symbols[rank.realModulo(map.symbols.length)]}
+                    {map.symbols[rankIndex.realModulo(map.symbols.length)]}
                 </p>
             </Modal.Body>
         </>
--- src/js/components/shared/GhanziSymbolsYiniYangModal.tsx
+++ src/js/components/shared/GhanziSymbolsYiniYangModal.tsx
@@ -9,6 +9,8 @@ const GhanziSymbolsYinYangModal: FunctionComponent = () => {
     const [state] = useApp();
     const rank: number = GetRank(state.dayOfYear < 36 ? state.year - 1 : state.year);
     const onNewYearRank: number = GetRank(state.isInNewYear ? state.year : state.year - 1);
+    const rankIndex: number = rank - 1;
+    const onNewYearRankIndex: number = onNewYearRank - 1;
 
     const map: SymbolsMap = symbolsmap[translator.locale];
     return (
@@ -27,9 +29,11 @@ const GhanziSymbolsYinYangModal: FunctionComponent = () => {
                             gānzhī
                         </Col>
                         <Col className={'text-center'}>
-                            {map.gan[rank.realModulo(map.gan.length)][0]} {map.zhi[rank.realModulo(map.zhi.length)][0]}
+                            {map.gan[rankIndex.realModulo(map.gan.length)][0]}{' '}
+                            {map.zhi[rankIndex.realModulo(map.zhi.length)][0]}
                             <br />
-                            {map.gan[rank.realModulo(map.gan.length)][1]} {map.zhi[rank.realModulo(map.zhi.length)][1]}
+                            {map.gan[rankIndex.realModulo(map.gan.length)][1]}{' '}
+                            {map.zhi[rankIndex.realModulo(map.zhi.length)][1]}
                         </Col>
                     </Row>
                     <Row className={'align-items-center'}>
@@ -39,46 +43,46 @@ const GhanziSymbolsYinYangModal: FunctionComponent = () => {
                             {translator.t('symbol.grand.meridien')}
                         </Col>
                         <Col className={'text-center'}>
-                            {map.energies[rank.realModulo(map.energies.length)][0]}
+                            {map.energies[rankIndex.realModulo(map.energies.length)][0]}
                             <br />
-                            {map.energies[rank.realModulo(map.energies.length)][1]}
+                            {map.energies[rankIndex.realModulo(map.energies.length)][1]}
                         </Col>
                     </Row>
                     <Row className={'align-items-center'}>
                         <Col className={'text-center'}>{translator.t('symbol.polarity')}</Col>
                         <Col className={'text-center'}>
-                            {map.polarities[rank.realModulo(map.polarities.length)][0]}
+                            {map.polarities[rankIndex.realModulo(map.polarities.length)][0]}
                             <br />
-                            {map.polarities[rank.realModulo(map.polarities.length)][1]}
+                            {map.polarities[rankIndex.realModulo(map.polarities.length)][1]}
                         </Col>
                     </Row>
                     <Row className={'align-items-center'}>
                         <Col className={'text-center'}>{translator.t('symbol.arrival.movement')}</Col>
                         <Col className={'text-center'}>
-                            {map.elements[rank.realModulo(map.elements.length)]} {translator.t('symbol.by')}{' '}
-                            {map.directions[rank.realModulo(map.directions.length)]}
+                            {map.elements[rankIndex.realModulo(map.elements.length)]} {translator.t('symbol.by')}{' '}
+                            {map.directions[rankIndex.realModulo(map.directions.length)]}
                         </Col>
                     </Row>
                     <Row className={'align-items-center'}>
                         <Col className={'text-center'}>{translator.t('symbol.astro.sign')}</Col>
                         <Col className={'text-center'}>
-                            {map.signs[onNewYearRank.realModulo(map.signs.length)][0]}
+                            {map.signs[onNewYearRankIndex.realModulo(map.signs.length)][0]}
                             <br />
-                            {map.signs[onNewYearRank.realModulo(map.signs.length)][1]}
+                            {map.signs[onNewYearRankIndex.realModulo(map.signs.length)][1]}
                         </Col>
                     </Row>
                     <Row className={'align-items-center'}>
                         <Col className={'text-center'}>{translator.t('symbol.element')}</Col>
                         <Col className={'text-center'}>
-                            {map.elementSigns[onNewYearRank.realModulo(map.elementSigns.length)][0]}
+                            {map.elementSigns[onNewYearRankIndex.realModulo(map.elementSigns.length)][0]}
                             <br />
-                            {map.elementSigns[onNewYearRank.realModulo(map.elementSigns.length)][1]}
+                            {map.elementSigns[onNewYearRankIndex.realModulo(map.elementSigns.length)][1]}
                         </Col>
                     </Row>
                     <Row className={'align-items-center'}>
                         <Col className={'text-center'}>{translator.t('symbol.orientation')}</Col>
                         <Col className={'text-center'}>
-                            {map.orientations[rank.realModulo(map.orientations.length)]}
+                            {map.orientations[rankIndex.realModulo(map.orientations.length)]}
                         </Col>
                     </Row>
                 </Container>
