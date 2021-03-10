1bc82ba493147eb79cb6f365f0d0ed0b438d60c7
Morel Sébastien <morel.seb@gmail.com>
2021-03-09 18:54:56 -0800

    Remove webp on safari

--- src/js/components/shared/ResponsiveImage.tsx
+++ src/js/components/shared/ResponsiveImage.tsx
@@ -1,5 +1,8 @@
 import React, { CSSProperties, FunctionComponent } from 'react';
 
+declare let window: any;
+declare let safari: any;
+
 const ResponsiveImage: FunctionComponent<{ src: string; className?: string; alt?: string; style?: CSSProperties }> = ({
     src,
     className,
@@ -11,10 +14,16 @@ const ResponsiveImage: FunctionComponent<{ src: string; className?: string; alt?
     const desktopPathWEBP = src.replace('/images/', '/images/desktop/webp/').replace('.png', '.webp');
     const mobilePathWEBP = src.replace('/images/', '/images/mobile/webp/').replace('.png', '.webp');
 
+    const isSafari =
+        /constructor/i.test(window.HTMLElement) ||
+        (function (p) {
+            return p.toString() === '[object SafariRemoteNotification]';
+        })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+
     return (
         <picture>
-            <source type="image/webp" srcSet={mobilePathWEBP} media="(max-width: 640px)" />
-            <source type="image/webp" srcSet={desktopPathWEBP} media="(min-width: 641px)" />
+            {!isSafari && <source type="image/webp" srcSet={mobilePathWEBP} media="(max-width: 640px)" />}
+            {!isSafari && <source type="image/webp" srcSet={desktopPathWEBP} media="(min-width: 641px)" />}
             <source srcSet={mobilePathPNG} media="(max-width: 640px)" />
             <source srcSet={desktopPathPNG} media="(min-width: 641px)" />
             <img loading="lazy" src="//:0" className={className} srcSet={mobilePathPNG} alt={alt} style={style} />
